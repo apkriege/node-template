69f788395a7605f0f578ac34c0842c0856f2c6cb
apkriege <apkriege@svsu.edu>
2018-01-24 14:04:59 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 3000;
+var path = require("path");
+var express = require("express");
+
+var app = express();
+app.use(express.static(__dirname + "/")); //use static files in ROOT/public folder
+
+app.get("/", function(req, res){ //root dir
+    res.sendFile(path.join(__dirname+'/index.html'))
+});
+
+app.get("/transfer", function(req, res){ //root dir
+    res.sendFile(path.join(__dirname+'/apps/transfer.html'))
+});
+
+//TODO add a callback that checks if already listening on that port
+app.listen(port, host, function (){
+  console.log('Server running on port:'+port);
+});
